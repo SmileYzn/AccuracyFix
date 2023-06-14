b14c9c3ac1a9ac600b5a145abebca351ee4722f9
SmileY <smileyznl@gmail.com>
2023-06-14 00:51:51 -0300

    Other test

--- AccuracyFix/AccuracyFix.cpp
+++ AccuracyFix/AccuracyFix.cpp
@@ -34,32 +34,27 @@ bool CAccuracyFix::TraceLine(const float* start, const float* end, int fNoMonste
 				{
 					if (!((BIT(WEAPON_NONE) | BIT(WEAPON_HEGRENADE) | BIT(WEAPON_XM1014) | BIT(WEAPON_C4) | BIT(WEAPON_SMOKEGRENADE) | BIT(WEAPON_M3) | BIT(WEAPON_FLASHBANG) | BIT(WEAPON_KNIFE)) & BIT(Player->m_pActiveItem->m_iId)))
 					{
-						CBasePlayerWeapon* Weapon = static_cast<CBasePlayerWeapon*>(Player->m_pActiveItem);
-
-						if (Weapon)
+						if ((Player->m_flLastFired - this->m_LastFired[EntityIndex]) > 0.8f)
 						{
-							if ((Player->m_flLastFired - this->m_LastFired[EntityIndex]) > 0.1f)
-							{
-								this->m_Shooting[EntityIndex] = true;
+							this->m_Shooting[EntityIndex] = true;
 
-								this->m_LastFired[EntityIndex] = Player->m_flLastFired;
+							this->m_LastFired[EntityIndex] = Player->m_flLastFired;
 
-								vec3_t vEnd;
+							vec3_t vEnd;
 
-								g_engfuncs.pfnMakeVectors(pentToSkip->v.v_angle);
+							g_engfuncs.pfnMakeVectors(pentToSkip->v.v_angle);
 
-								vEnd = gpGlobals->v_forward * 9999.0f;
+							vEnd = gpGlobals->v_forward * 9999.0f;
 
-								vEnd[0] = start[0] + vEnd[0];
+							vEnd[0] = start[0] + vEnd[0];
 
-								vEnd[1] = start[1] + vEnd[1];
+							vEnd[1] = start[1] + vEnd[1];
 
-								vEnd[2] = start[2] + vEnd[2];
+							vEnd[2] = start[2] + vEnd[2];
 
-								g_engfuncs.pfnTraceLine(start, vEnd, fNoMonsters, pentToSkip, ptr);
+							g_engfuncs.pfnTraceLine(start, vEnd, fNoMonsters, pentToSkip, ptr);
 
-								return true;
-							}
+							return true;
 						}
 					}
 				}
@@ -88,9 +83,7 @@ void CAccuracyFix::POST_CBasePlayer_PostThink(CBasePlayer* Player)
 
 					auto PunchAngle = Player->edict()->v.punchangle;
 
-					//PunchAngle = PunchAngle * Weapon->m_flAccuracy;
-
-					PunchAngle = PunchAngle * 0.237f;
+					PunchAngle = PunchAngle * Weapon->m_flAccuracy;
 
 					Player->edict()->v.punchangle = PunchAngle;
 				}
