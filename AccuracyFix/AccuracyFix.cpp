c4a541fc587913f7301fbf5e2bbb66b53b8c0dc6
Adrian Cirstea <54354938+ShadowsAdi@users.noreply.github.com>
2024-06-15 22:22:28 +0300

    #NeverNestCode

--- AccuracyFix/AccuracyFix.cpp
+++ AccuracyFix/AccuracyFix.cpp
@@ -18,44 +18,38 @@ void CAccuracyFix::ServerActivate()
 	{
 		auto SlotInfo = g_ReGameApi->GetWeaponSlot((WeaponIdType)WeaponID);
 
-		if (SlotInfo)
-		{
-			if ((SlotInfo->slot == PRIMARY_WEAPON_SLOT) || (SlotInfo->slot == PISTOL_SLOT))
-			{
-				if (SlotInfo->weaponName)
-				{
-					if (SlotInfo->weaponName[0u] != '\0')
-					{
-						char cvarName[64] = { 0 };
+		if (!SlotInfo)
+			continue;
 
-						Q_snprintf(cvarName, sizeof(cvarName), "af_distance_%s", SlotInfo->weaponName);
+		if (!((SlotInfo->slot == PRIMARY_WEAPON_SLOT) || (SlotInfo->slot == PISTOL_SLOT)))
+			continue;
 
-						this->m_af_distance[WeaponID] = gAccuracyUtil.CvarRegister(cvarName, "8192.0");
+		if (!SlotInfo->weaponName || SlotInfo->weaponName[0u] == '\0')
+			continue;
 
-						Q_snprintf(cvarName, sizeof(cvarName), "af_accuracy_%s", SlotInfo->weaponName);
+		char cvarName[64] = { 0 };
 
-						this->m_af_accuracy[WeaponID] = gAccuracyUtil.CvarRegister(cvarName, "9999.0");
+		Q_snprintf(cvarName, sizeof(cvarName), "af_distance_%s", SlotInfo->weaponName);
+
+		this->m_af_distance[WeaponID] = gAccuracyUtil.CvarRegister(cvarName, "8192.0");
+
+		Q_snprintf(cvarName, sizeof(cvarName), "af_accuracy_%s", SlotInfo->weaponName);
+
+		this->m_af_accuracy[WeaponID] = gAccuracyUtil.CvarRegister(cvarName, "9999.0");
 
 #ifdef ACCURACY_ENABLE_RECOIL_CONTROL
-						Q_snprintf(cvarName, sizeof(cvarName), "af_recoil_%s", SlotInfo->weaponName);
+		Q_snprintf(cvarName, sizeof(cvarName), "af_recoil_%s", SlotInfo->weaponName);
 
-						this->m_af_recoil[WeaponID] = gAccuracyUtil.CvarRegister(cvarName, "1.0");
+		this->m_af_recoil[WeaponID] = gAccuracyUtil.CvarRegister(cvarName, "1.0");
 #endif
-					}
-				}
-			}
-		}
 	}
 
 	auto Path = gAccuracyUtil.GetPath();
 
-	if (Path)
-	{
-		if (Path[0u] != '\0')
-		{
-			gAccuracyUtil.ServerCommand("exec %s/accuracyfix.cfg", Path);
-		}
-	}
+	if (!Path || Path[0u] == '\0')
+		return;
+
+	gAccuracyUtil.ServerCommand("exec %s/accuracyfix.cfg", Path);
 }
 
 #ifdef ACCURACY_ENABLE_RECOIL_CONTROL
@@ -63,112 +57,102 @@ void CAccuracyFix::CmdEnd(const edict_t* pEdict)
 {
 	auto Player = UTIL_PlayerByIndexSafe(ENTINDEX(pEdict));
 
-	if (Player)
-	{
-		if (Player->IsAlive())
-		{
-			this->m_Data[Player->entindex()].LastFired = Player->m_flLastFired;
-		}
-	}
+	if (!Player)
+		return;
+
+	if (!Player->IsAlive())
+		return;
+
+	this->m_Data[Player->entindex()].LastFired = Player->m_flLastFired;
 }
 #endif
 
 void CAccuracyFix::TraceLine(const float* vStart, const float* vEnd, int fNoMonsters, edict_t* pentToSkip, TraceResult* ptr)
 {
-	if (fNoMonsters == dont_ignore_monsters)
+	if (fNoMonsters != dont_ignore_monsters || FNullEnt(pentToSkip))
+		return;
+
+	auto EntityIndex = g_engfuncs.pfnIndexOfEdict(pentToSkip);
+
+	if (!(EntityIndex > 0 && EntityIndex <= gpGlobals->maxClients))
+		return;
+
+	auto Player = UTIL_PlayerByIndexSafe(EntityIndex);
+
+	if (!Player || !Player->IsAlive() || !Player->m_pActiveItem || 
+		!((Player->m_pActiveItem->iItemSlot() == PRIMARY_WEAPON_SLOT) || (Player->m_pActiveItem->iItemSlot() == PISTOL_SLOT)))
 	{
-		if (!FNullEnt(pentToSkip))
-		{
-			auto EntityIndex = g_engfuncs.pfnIndexOfEdict(pentToSkip);
-
-			if (EntityIndex > 0 && EntityIndex <= gpGlobals->maxClients)
-			{
-				auto Player = UTIL_PlayerByIndexSafe(EntityIndex);
-
-				if (Player)
-				{
-					if (Player->IsAlive())
-					{
-						if (Player->m_pActiveItem)
-						{
-							if ((Player->m_pActiveItem->iItemSlot() == PRIMARY_WEAPON_SLOT) || (Player->m_pActiveItem->iItemSlot() == PISTOL_SLOT))
-							{
-#ifdef ACCURACY_ENABLE_RECOIL_CONTROL
-								if ((Player->edict()->v.button & IN_ATTACK) && (Player->m_flLastFired != this->m_Data[EntityIndex].LastFired))
-								{
-									this->m_Data[EntityIndex].LastFired = Player->m_flLastFired;
-
-									this->m_Data[EntityIndex].WeaponId = Player->m_pActiveItem->m_iId;
-								}
-#endif
-								auto DistanceLimit = this->m_af_distance[Player->m_pActiveItem->m_iId]->value;
-
-								if (this->m_af_distance_all->value > 0)
-								{
-									DistanceLimit = this->m_af_distance_all->value;
-								}
-
-								if (DistanceLimit > 0.0f)
-								{
-									auto trResult = gAccuracyUtil.GetUserAiming(pentToSkip, DistanceLimit);
-
-									if (!FNullEnt(trResult.pHit))
-									{
-										auto TargetIndex = ENTINDEX(trResult.pHit);
-
-										if (TargetIndex > 0 && TargetIndex <= gpGlobals->maxClients)
-										{
-											auto fwdVelocity = this->m_af_accuracy[Player->m_pActiveItem->m_iId]->value;
-
-											if (this->m_af_accuracy_all->value > 0.0f)
-											{
-												fwdVelocity = this->m_af_accuracy_all->value;
-											}
-
-											g_engfuncs.pfnMakeVectors(pentToSkip->v.v_angle);
-
-											auto vEndRes = (Vector)vStart + gpGlobals->v_forward * fwdVelocity;
-
-											g_engfuncs.pfnTraceLine(vStart, vEndRes, fNoMonsters, pentToSkip, ptr);
-										}
-									}
-								}
-							}
-						}
-					}
-				}
-			}
-		}
+		return;
 	}
+
+#ifdef ACCURACY_ENABLE_RECOIL_CONTROL
+	if (!((Player->edict()->v.button & IN_ATTACK) && (Player->m_flLastFired = this->m_Data[EntityIndex].LastFired)))
+		return;
+
+	this->m_Data[EntityIndex].LastFired = Player->m_flLastFired;
+
+	this->m_Data[EntityIndex].WeaponId = Player->m_pActiveItem->m_iId;
+#endif
+	auto DistanceLimit = this->m_af_distance[Player->m_pActiveItem->m_iId]->value;
+
+	if (this->m_af_distance_all->value > 0)
+	{
+		DistanceLimit = this->m_af_distance_all->value;
+	}
+
+	if (!(DistanceLimit > 0.0f)
+		return;
+
+	auto trResult = gAccuracyUtil.GetUserAiming(pentToSkip, DistanceLimit);
+
+	if (FNullEnt(trResult.pHit)))
+		return;
+
+	auto TargetIndex = ENTINDEX(trResult.pHit);
+
+	if (!(TargetIndex > 0 && TargetIndex <= gpGlobals->maxClients))
+		return;
+
+	auto fwdVelocity = this->m_af_accuracy[Player->m_pActiveItem->m_iId]->value;
+
+	if (this->m_af_accuracy_all->value > 0.0f)
+	{
+		fwdVelocity = this->m_af_accuracy_all->value;
+	}
+
+	g_engfuncs.pfnMakeVectors(pentToSkip->v.v_angle);
+
+	auto vEndRes = (Vector)vStart + gpGlobals->v_forward * fwdVelocity;
+
+	g_engfuncs.pfnTraceLine(vStart, vEndRes, fNoMonsters, pentToSkip, ptr);
 }
 
 #ifdef ACCURACY_ENABLE_RECOIL_CONTROL
 void CAccuracyFix::PostThink(CBasePlayer* Player)
 {
-	if (Player->IsAlive())
+	if (!Player->IsAlive())
+		return;
+	auto EntityIndex = Player->entindex();
+
+	if (this->m_Data[EntityIndex].WeaponId == WEAPON_NONE)
+		return;
+	
+	auto Recoil = this->m_af_recoil[this->m_Data[EntityIndex].WeaponId]->value;
+
+	if (Recoil > 0.0f)
 	{
-		auto EntityIndex = Player->entindex();
-
-		if (this->m_Data[EntityIndex].WeaponId != WEAPON_NONE)
+		if (this->m_af_recoil_all->value > 0.0f)
 		{
-			auto Recoil = this->m_af_recoil[this->m_Data[EntityIndex].WeaponId]->value;
-
-			if (Recoil > 0.0f)
-			{
-				if (this->m_af_recoil_all->value > 0.0f)
-				{
-					Recoil = this->m_af_recoil_all->value;
-				}
-
-				auto PunchAngle = Player->edict()->v.punchangle;
-
-				PunchAngle = PunchAngle * Recoil;
-
-				Player->edict()->v.punchangle = PunchAngle;
-			}
-
-			this->m_Data[EntityIndex].WeaponId = WEAPON_NONE;
+			Recoil = this->m_af_recoil_all->value;
 		}
+
+		auto PunchAngle = Player->edict()->v.punchangle;
+
+		PunchAngle = PunchAngle * Recoil;
+
+		Player->edict()->v.punchangle = PunchAngle;
 	}
+
+	this->m_Data[EntityIndex].WeaponId = WEAPON_NONE;
 }
 #endif
