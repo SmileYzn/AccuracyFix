eacb7f1d64c5df237ffe7a14ce80b7a8765c850b
Cleverson <SmileYzn@users.noreply.github.com>
2024-08-23 11:32:40 -0300

    Update AccuracyFix.h

--- AccuracyFix/AccuracyFix.h
+++ AccuracyFix/AccuracyFix.h
@@ -1,5 +1,13 @@
 #pragma once
 
+/**
+* global_get(glb_trace_flags) values
+* @note Custom flags that we can retrieve in pfnShouldCollide/TraceLine/TraceHull
+*/
+#define FTRACE_BULLET                   (1<<16)
+#define FTRACE_FLASH                    (1<<17)
+#define FTRACE_KNIFE                    (1<<18)
+
 class CAccuracyFix
 {
 public:
